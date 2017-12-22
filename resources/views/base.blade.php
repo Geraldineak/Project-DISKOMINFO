b2ba31cf1d82bc1caf35ffd9f16064839de74c80
Geraldine Agusta <geraldineak@gmail.com>
2017-12-22 22:54:28 +0700

    TEMPLATING

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="resources/assets/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="resources/assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="resources/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="resources/assets/css/font-awesome.css">
+    <link rel="stylesheet" href="resources/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="resources/assets/css/style.css">
 </head>
 <body>
 <nav class="navbar navbar-default">
